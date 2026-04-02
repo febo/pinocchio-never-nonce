864f648b0ea697a1b0a6bde0f9a0bf1e58ad9393
febo <febo@anza.xyz>
2026-04-02 21:13:59 +0300

    Add macro crate

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -1,15 +1,12 @@
 #![no_std]
 
+use p_never_nonce::never_nonce;
 use pinocchio::{
-    Address, ProgramResult,
+    ProgramResult,
     entrypoint::{InstructionContext, MaybeAccount},
     error::ProgramError,
     lazy_program_entrypoint, no_allocator, nostd_panic_handler,
-    sysvars::instructions::Instructions,
 };
-
-// System program ID.
-const SYSTEM_PROGRAM_ID: Address = Address::new_from_array([0u8; 32]);
 
 // Disable the memory allocator.
 no_allocator!();
@@ -23,16 +20,7 @@ pub fn process_instruction(mut context: InstructionContext) -> ProgramResult {
         return Err(ProgramError::NotEnoughAccountKeys);
     };
 
-    let sysvar = Instructions::try_from(&account)?;
-    // Load the first top-level instruction.
-    let instruction = sysvar.load_instruction_at(0)?;
-    // Reject the transaction if the first instruction  is a system program
-    // `AdvanceNonceAccount` instruction.
-    if instruction.get_program_id() == &SYSTEM_PROGRAM_ID
-        && let Some([4, 0, 0, 0]) = instruction.get_instruction_data().get(..4)
-    {
-        return Err(ProgramError::InvalidArgument);
-    }
+    never_nonce!(account, ProgramError::InvalidArgument);
 
     Ok(())
 }
